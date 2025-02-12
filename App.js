89004d0c14d0d21d17fadf3755525d49730ee225
nunnaram <nunna.sreeramamurthy1@gmail.com>
2025-02-12 08:54:04 +0530

    Chapter-01

---
+++ App.js
@@ -0,0 +1,27 @@
+const heading = React.createElement('h1', {id:'heading'}, 'Hello World from React');
+console.log(heading);
+
+const parent = React.createElement(
+    'div',
+    {id:'parent'},
+    [
+        React.createElement(
+            'div',
+            {id:'child1'},
+            [
+                React.createElement("h1",{},"I'm an H1 tag"),
+                React.createElement("h2",{},"I'm an H2 tag")
+            ]
+        ),
+        React.createElement(
+            'div',
+            {id:'child2'},
+            [
+                React.createElement("h1",{},"I'm an H1 tag"),
+                React.createElement("h2",{},"I'm an H2 tag")
+            ]
+        )
+    ]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
